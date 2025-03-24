1f69f72263e5c0e569d5533c85d830ab9d348672
nulltiton <nborisov076@gmail.com>
2025-03-24 09:22:47 +0300

    Updates

--- src/components/AsteroidCard/AsteroidCardContent/AsteroidCardContentContainer.tsx
+++
@@ -1,8 +0,0 @@
-import {AsteroidCardContent} from "./AsteroidCardContent";
-import {useContext} from "react";
-import {AsteroidsContext} from "../../AsteroidsСontext/AsteroidsContext";
-
-export const AsteroidCardContentContainer = (props)=> {
-    const {distanceMode} = useContext(AsteroidsContext);
-    return <AsteroidCardContent {...props} distanceMode={distanceMode} />
-}
--- src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Footer.css';
-
-export const Footer = () => {
-    return (
-        <div className={styles.bottom}>
-            2021 © Все права и планета защищены
-        </div>
-    );
-};
